ec80256d45a361428b5c788310e8520c7f827544
maguowei <maguowei@meituan.com>
2019-06-03 14:13:46 +0800

    ci(repo): :green_heart:recover

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@gitlab.com:wheatma/grid-trade-helper.git master:gh-pages
+git push -f git@github.com:wheatma/grid-trade-helper.git master:gh-pages
 
 cd -
