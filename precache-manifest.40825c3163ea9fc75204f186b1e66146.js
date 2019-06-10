d82b135d7637abe6350b99888280b35a02aea5dd
maguowei <maguowei@meituan.com>
2019-06-10 13:54:27 +0800

    deploy

---
+++ precache-manifest.40825c3163ea9fc75204f186b1e66146.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "e64c7ae9e1e36f585273",
+    "url": "/grid-trade-helper/css/app.e4c43d99.css"
+  },
+  {
+    "revision": "e64c7ae9e1e36f585273",
+    "url": "/grid-trade-helper/js/app.bbbe7186.js"
+  },
+  {
+    "revision": "045e037677a2fbabafda",
+    "url": "/grid-trade-helper/css/chunk-vendors.af67b8ba.css"
+  },
+  {
+    "revision": "045e037677a2fbabafda",
+    "url": "/grid-trade-helper/js/chunk-vendors.975b77d8.js"
+  },
+  {
+    "revision": "daed72485d0014540ebe9e8173ab7c64",
+    "url": "/grid-trade-helper/img/slogan.daed7248.png"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/grid-trade-helper/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/grid-trade-helper/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "6be0dbe9b98634d44acca9f28d45f82c",
+    "url": "/grid-trade-helper/index.html"
+  },
+  {
+    "revision": "daed72485d0014540ebe9e8173ab7c64",
+    "url": "/grid-trade-helper/img/slogan.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/grid-trade-helper/robots.txt"
+  },
+  {
+    "revision": "e0faf6205cef05b8d68f551ac20b1436",
+    "url": "/grid-trade-helper/img/logo.png"
+  }
+];
